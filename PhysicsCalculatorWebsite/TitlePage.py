4eb4942b1f8b61fbefe01a5f62fdb33213b556cc
Ved Jaggi <vedajaggi@gmail.com>
2025-03-23 11:57:34 +0800

    step 16
    added a display method to display small text for title screen
    changes: added newline characters to make write more asthetic. changed
    display to be static, because it does not depend on class

--- PhysicsCalculatorWebsite/TitlePage.py
+++ PhysicsCalculatorWebsite/TitlePage.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
 class DisplayTitle:
-    def display(self):
-        pass
+    @staticmethod
+    def display():
+        st.write(
+            "This is a calculator that contains a variety of different Physics equations including Kinetic Energy, Pivot Moments, Force, and a Rate of Work calculator"
+        )
