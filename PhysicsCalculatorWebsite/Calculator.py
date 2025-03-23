f8a3c305a504a7ac4f889f7d97d3d32e76519446
Ved Jaggi <vedajaggi@gmail.com>
2025-03-23 16:02:26 +0800

    step 24
    added function
    changes: changed variable names to be pythonic and follow python best
    practices, but mainly enhance readability

---
+++ PhysicsCalculatorWebsite/Calculator.py
@@ -0,0 +1,11 @@
+def CalculateKineticEnergy(mass, velocity):
+    return 0.5 * mass * velocity ** 2
+
+def CalculatePivotMoments(distance_from_pivot, force_exerted):
+    return distance_from_pivot * force_exerted
+
+def CalculateForce(mass, acceleration):
+    return mass * acceleration
+
+def CalculateRateOfWork(energy_transferred, time_transferred):
+    return energy_transferred / time_transferred
