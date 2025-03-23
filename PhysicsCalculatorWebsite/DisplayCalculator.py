cc5eb986f82cfe0b9e5335c95a9b79784eb42cc0
Ved Jaggi <vedajaggi@gmail.com>
2025-03-23 15:42:01 +0800

    Step 22
    Added calculate button
    changes: None

--- PhysicsCalculatorWebsite/DisplayCalculator.py
+++ PhysicsCalculatorWebsite/DisplayCalculator.py
@@ -7,6 +7,10 @@ class DisplayCalculator:
         value_1 = left_column.number_input("Placeholder Prompt", max_value=100.0, min_value=0.0, key="KineticEnergy1")
         value_2 = right_column.number_input("Placeholder Prompt", max_value=100.0, min_value=0.0, key="KineticEnergy2")
 
+        val = st.button("Calculate", key="KineticEnergy")
+        if val:
+            answer_box = st.container(border=True)
+            answer_box.write(f"Placeholder {""} Placeholder")
 
     @staticmethod
     def PivotMoments() -> None:
