d68e17dba11e593a569f078f1a6d37512f7c06b3
Ved Jaggi <vedajaggi@gmail.com>
2025-03-23 13:05:48 +0800

    step 21
    added template for inputting question
    changes: had to add keys so output values would not overlap, and I had
    to input maximum, miminimum and step values for number_input to work. I
    remove st.write(), because number_input has its own prompt

--- PhysicsCalculatorWebsite/DisplayCalculator.py
+++ PhysicsCalculatorWebsite/DisplayCalculator.py
@@ -3,7 +3,10 @@ import streamlit as st
 class DisplayCalculator:
     @staticmethod
     def KineticEnergy() -> None:
-        pass
+        left_column, right_column = st.columns(2)
+        value_1 = left_column.number_input("Placeholder Prompt", max_value=100.0, min_value=0.0, key="KineticEnergy1")
+        value_2 = right_column.number_input("Placeholder Prompt", max_value=100.0, min_value=0.0, key="KineticEnergy2")
+        # value_2 = right_column.number_input("Placeholder", step=0.1, max_value=100.0, min_value=0.0)
 
     @staticmethod
     def PivotMoments() -> None:
