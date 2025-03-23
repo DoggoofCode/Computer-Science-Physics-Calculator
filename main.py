a9df0cbf9fea14ba2234c02bebbdfdc75a6edb13
Ved Jaggi <vedajaggi@gmail.com>
2025-03-23 11:51:51 +0800

    step 13
    added tabs

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ def main():
     st.set_page_config(page_title="Physics Calculator", page_icon=":rocket:", layout="wide")
     st.title("Physics Calculator")
 
+    TitleScreen, KineticEnergy, PivotMoments, NewtonsForce, RateOfWorkDone = st.tabs(["Title Screen", "Kinetic Energy", "Pivot Moments", "Newton’s Force", "Rate of work done"])
+
 if __name__ == "__main__":
     main()
